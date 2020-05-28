4834ac37abe7eb94747f7a2feb1ddb4e7bdaffd4
Iwan Trofimtschuk <iwan@djangsters.de>
2020-05-28 14:35:08 +0200

    Fix typo

--- rt_playground/dashboard.py
+++ rt_playground/dashboard.py
@@ -14,5 +14,5 @@ def application(environ, start_response):
 if __name__ == '__main__':
     host = os.environ.get("HOST", "localhost")
     port = int(os.environ.get("PORT", 8080))
-    user_reloader = bool(os.environ.get("USE_RELOADER", True))
+    use_reloader = bool(os.environ.get("USE_RELOADER", True))
     run_simple(host, port, application, use_reloader=use_reloader)
