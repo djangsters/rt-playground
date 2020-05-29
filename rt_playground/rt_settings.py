5dcce448423a4bb105aefa815f3c8254d3fadde8
Tatiana Vasilevskaya <vasilty@gmail.com>
2020-05-29 11:15:32 +0200

    Allow success task to be on the queue multiple times

--- rt_playground/rt_settings.py
+++ rt_playground/rt_settings.py
@@ -5,12 +5,13 @@ REDIS_PREFIX = "rt_playground"
 SCHEDULE = {
     "success": {
         "task": "rt_playground.tasks.success",
+        "singleton": False,
         "args": [40],
         "schedule": run_every(seconds=30),
     },
     "failure": {
         "task": "rt_playground.tasks.failure",
         "kwargs": {"sleep_time": 5},
-        "schedule": run_every(minutes=1)
+        "schedule": run_every(minutes=1),
     }
 }
