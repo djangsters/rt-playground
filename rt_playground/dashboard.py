0536aebde8b3dcf53afbfee28e70a7781d962066
Tatiana Vasilevskaya <vasilty@gmail.com>
2020-05-29 12:52:04 +0200

    Update rt_playground/dashboard.py
    Co-authored-by: Iwan Trofimtschuk <iwan@djangsters.de>

--- rt_playground/dashboard.py
+++ rt_playground/dashboard.py
@@ -14,5 +14,5 @@ def application(environ, start_response):
 if __name__ == '__main__':
     host = os.environ.get("HOST")
     port = int(os.environ.get("PORT"))
-    use_reloader = bool(os.environ.get("USE_RELOADER", True))
+    use_reloader = bool(int(os.environ.get("USE_RELOADER", "1")))
     run_simple(host, port, application, use_reloader=use_reloader)
